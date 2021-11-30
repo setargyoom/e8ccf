e843129e4de7c94aadc442982c3235fa91e80f56
setargyoom <81277009+setargyoom@users.noreply.github.com>
2021-12-01 06:14:55 +0700

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,6 @@ import Link from 'next/link'
 export default function FourOhFour() {
   return <>
    <center>
-    ![Alt text](public/404.svg?raw=true "404")
     <h1>404 - Page Not Found</h1>
     <Link href="/">
       <a>
