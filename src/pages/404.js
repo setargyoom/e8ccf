56d04f2b70b0ce9d106932b9a551e9b59feb6d1a
setargyoom <81277009+setargyoom@users.noreply.github.com>
2021-12-01 06:18:38 +0700

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,6 +4,10 @@ import Link from 'next/link'
 export default function FourOhFour() {
   return <>
    <center>
+    var img = document.createElement("img");
+img.src = "/images/404.svg";
+var src = document.getElementById("header");
+src.appendChild(img);
     <h1>404 - Page Not Found</h1>
     <Link href="/">
       <a>
