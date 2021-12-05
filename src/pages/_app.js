8af787e1f24e0c1f9606ec0ae296ede3d41d59a0
setargyoom <81277009+setargyoom@users.noreply.github.com>
2021-12-05 14:05:04 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,106 @@
 import '../sass/main.scss';
+import 
+          'styles/global.scss'
+          ;
+         
+
+         
+ 
+         
+
+         
+
+          import 
+          { ThemeProvider } 
+          from 
+          'next-themes'
+          ;
+         
+
+         
+
+          import 
+          { MDXProvider } 
+          from 
+          '@mdx-js/react'
+          ;
+         
+
+         
+
+          import 
+          MDXComponents 
+          from 
+          'components/MDXComponents'
+          ;
+         
+
+         
+ 
+         
+
+         
+
+          function 
+          MyApp
+          (
+          { Component, pageProps }
+          ) 
+          {
+         
+
+         
+ 
+          return 
+          (
+         
+
+         
+ 
+          <ThemeProvider attribute="class">
+         
+
+         
+ 
+          <MDXProvider components={MDXComponents}>
+         
+
+         
+ 
+          <Component {...pageProps} />
+         
+
+         
+ 
+          </MDXProvider>
+         
+
+         
+ 
+          </ThemeProvider>
+         
+
+         
+ 
+          )
+          ;
+         
+
+         
+
+          }
+         
+
+         
+ 
+         
+
+         
+
+          export 
+          default 
+          MyApp
+          ;
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
