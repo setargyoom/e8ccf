bbd94ade038952c6137f2cb551288f478b2dc158
setargyoom <81277009+setargyoom@users.noreply.github.com>
2023-03-13 10:30:09 +0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -35,7 +35,8 @@ export default class MyDocument extends Document {
          
                       <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2378860167138114"
      crossorigin="anonymous"></script>
-
+                          
+                       <meta name="google-site-verification" content="wUeM07dkLdU-2K147mThUGaAhsD7vvHuqsNN_KeohRc" />
                       
                            
                            </Head>
